9fda6a78ebcfdcab76bc6b2204f17cde9eba35e8
Iman Tabrizian <tabrizian@outlook.com>
2016-05-12 17:45:00 +0430

    Adds exercise files.

---
+++ exercise_files/sort.h
@@ -0,0 +1,13 @@
+#ifndef Q1_SORT_H
+#define Q1_SORT_H
+
+//Sorts array using bubble sort
+void merge_sort(int *arr, int size);
+
+void insertion_sort(int *arr, int size);
+
+void heap_sort(int *arr, int size);
+
+void quick_sort(int *arr, int size);
+
+#endif //Q1_SORT_H
